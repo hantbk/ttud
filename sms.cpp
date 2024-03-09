7e2aa22bd766369e2bb2d66fc90fea3b11ee9ecc
Ha Nguyen <captainnemot1k60@gmail.com>
2024-03-09 11:44:04 +0700

    ok

--- sms.cpp
+++ sms.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int n;
+int test = 0;
 string s;
 int arr[26] = {1,2,3,
                1,2,3,
@@ -11,8 +13,13 @@ int arr[26] = {1,2,3,
                1,2,3,
                1,2,3,4     
                };
+string sKey[12] = {
+    " ", "abc", "def",
+    "ghi", "jkl", "mno",
+    "pqrs", "tuv", "wxyz",
+    " ", " ", " "};
 
-void solve(){
+void solve1(){
     string s;
     getline(cin,s);
     int sum = 0;
@@ -27,14 +34,37 @@ void solve(){
     cout<<sum<<"\n";
 }
 
-int main() {
+void solve2() {
+    string sLine;
+    getline(cin, sLine);
+    int result = 0;
+    for (int i = 0; i < sLine.size(); i++)
+    {
+        int curr = 0;
+        for (int j = 0; j < 12; j++)
+        {
+            for (int k = 0; k < sKey[j].size(); k++)
+            {
+                if (sLine[i] == sKey[j][k])
+                {
+                    curr = k + 1;
+                }
+                
+            }
+            
+        }
+        result +=curr;
+    }
+    cout<<"Case "<<test<<" : "<<result<<endl;
+}
 
-    int n;
+int main() {
     cin >> n;
     getline(cin,s);
     while (n--)
     {
-        solve();
+        test++;
+        solve2();
     }
     return 0;
 }
