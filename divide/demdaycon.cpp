f816b5c91402da17f6e5920c972e752d3eaf0bec
Ha Nguyen <captainnemot1k60@gmail.com>
2024-04-16 17:00:47 +0700

    Refactor demdaycon.cpp to improve readability and add user input validation

--- divide/demdaycon.cpp
+++ divide/demdaycon.cpp
@@ -1,18 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define INF 1e9
 #define MAX 100000
 #define ll long long
 #define MOD 1000000007
 
+int n, a[MAX], S;
 
+int F(int tong, int n)
+{
+    // Tinh so day con co tong bang S
+    if (n == 0)
+        if (tong == 0)
+            return 1;
+        else
+            return 0;
+    else 
+        return F(tong, n - 1) + F(tong - a[n], n - 1);
+}
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
-    
+    cout << "Nhap n: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << "Nhap a[" << i << "]: ";
+        cin >> a[i];
+    }
+    cout << "Nhap S: ";
+    cin >> S;
+    cout << "Cac day con co tong bang S la: " << F(S, n) << endl;
 
     return 0;
 }
